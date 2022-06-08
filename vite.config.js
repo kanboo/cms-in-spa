afabf5d7a9ce76f0e016bb487fbfaaf8ed2aa254
Kanboo <kanboowang@17.media>
2022-06-08 10:22:29 +0800

    更新 vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
 import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-undef
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    // eslint-disable-next-line no-undef
+    base: process.env.NODE_ENV === "production" ? "/cms-in-spa/" : "./",
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+  };
 });
